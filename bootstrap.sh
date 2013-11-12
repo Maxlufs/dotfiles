a6b97e3cfbf0b17e710163e904738eee5dc511f0
Maxlufs <maxlufs@hotmail.com>
2013-11-12 05:59:24 -0800

    Update stdout width to 60 @bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,6 +21,7 @@
 # M: add bash 3.00 support                                                  #
 # M: prompt users and ask if they what to git push for backup               #
 # S: add report of system default env, eg. uname,bash,etc                   #
+# S: git push backup (only when there is change in git diff)                #
 # S: git push backup (based on if ssh keys are generated)                   #
 # S: copy vim :helptags locations                                           #
 # S: only link files w/o extensions                                         #
