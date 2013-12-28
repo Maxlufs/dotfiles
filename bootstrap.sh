768e311e924d5e462bd6d2d244bff0d1e60f6978
Maxlufs <maxlufs@hotmail.com>
2013-12-28 12:29:49 -0800

    Add ctrlp repo @bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -112,6 +112,7 @@ repo["nerdtree"]="https://github.com/scrooloose/nerdtree.git"
 repo["powerline"]="https://github.com/Lokaltog/powerline.git"
 repo["airline"]="https://github.com/bling/vim-airline.git"
 repo["fugitive"]="https://github.com/tpope/vim-fugitive.git"
+repo["ctrlp"]="https://github.com/kien/ctrlp.vim.git"
 ###########################################################################
 
 ###########################################################################
