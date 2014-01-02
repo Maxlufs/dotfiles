8666db35accbf461d21bb74bbd06290f78d4273f
Maxlufs <maxlufs@hotmail.com>
2014-01-02 01:38:16 -0800

    Add smart tabs repo @bootstrap.sh
    Add this plugin back to do some testing.

--- bootstrap.sh
+++ bootstrap.sh
@@ -120,7 +120,7 @@ repo["fugitive"]="https://github.com/tpope/vim-fugitive.git"
 repo["ctrlp"]="https://github.com/kien/ctrlp.vim.git"
 # repo["indentguides"]="https://github.com/nathanaelkane/vim-indent-guides.git"
 # use listchars instead
-# repo["smarttabs"]="https://github.com/vim-scripts/Smart-Tabs.git"
+repo["smarttabs"]="https://github.com/vim-scripts/Smart-Tabs.git"
 # has conflict with listchars
 repo["surround"]="https://github.com/tpope/vim-surround.git"
 
