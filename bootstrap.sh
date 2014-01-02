5808faa2e84a9b4bc9fcee9284300d77d3be40e5
Maxlufs <maxlufs@hotmail.com>
2014-01-01 20:05:26 -0800

    Com whitespaces layout spelling @bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -43,7 +43,7 @@ OLDDIR=~/.dotfiles_old          # dotfiles backup dir
 VIMDIR=vim                      # vim dir
 BUNDLEDIR=vim/bundle            # vim plugin dir
 
-# log_msg() function 
+# log_msg() function
 # input  : (STATUS_MSG, COLOR, PREV_MSG)
 # output : colored status in the same line with MSG
 #=========================================================================#
@@ -83,7 +83,7 @@ log_msg() {
     # STATUS = text
     # COLORSTATUS = wrapped text
 
-    if [[ ${#MSG} -ge ${COL} ]] 
+    if [[ ${#MSG} -ge ${COL} ]]
         # if MSG length too long, then print in new line
     then
         let COLFORLONG=$MAXCOL+${#COLORSTATUS}-$OFFSET-${#STATUS}
@@ -96,7 +96,10 @@ log_msg() {
 
 # Declare vim plugin repo associate matrix
 #=========================================================================#
-declare -A repo                 
+declare -A repo
+
+# vim pathogen plugin
+PATHOGENREPO="https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim"
 
 # vim colorschemes
 repo["jellybeans"]="https://github.com/nanotech/jellybeans.vim.git"
@@ -104,12 +107,15 @@ repo["molokai"]="https://github.com/tomasr/molokai.git"
 repo["wombat"]="https://github.com/vim-scripts/Wombat.git "
 repo["wombat256"]="https://github.com/vim-scripts/wombat256.vim.git"
 
-# vim plugins
-PATHOGENREPO="https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim"
-repo["easymotion"]="https://github.com/Lokaltog/vim-easymotion.git"
+# vim ui plugins
 repo["nerdtree"]="https://github.com/scrooloose/nerdtree.git"
-repo["powerline"]="https://github.com/Lokaltog/powerline.git"
+repo["gundo"]="http://github.com/sjl/gundo.vim.git"
+# repo["powerline"]="https://github.com/Lokaltog/powerline.git"
+# use airline instead
 repo["airline"]="https://github.com/bling/vim-airline.git"
+
+# vim plugins
+repo["easymotion"]="https://github.com/Lokaltog/vim-easymotion.git"
 repo["fugitive"]="https://github.com/tpope/vim-fugitive.git"
 repo["ctrlp"]="https://github.com/kien/ctrlp.vim.git"
 # repo["indentguides"]="https://github.com/nathanaelkane/vim-indent-guides.git"
@@ -117,9 +123,11 @@ repo["ctrlp"]="https://github.com/kien/ctrlp.vim.git"
 # repo["smarttabs"]="https://github.com/vim-scripts/Smart-Tabs.git"
 # has conflict with listchars
 repo["surround"]="https://github.com/tpope/vim-surround.git"
-repo["gundo"]="http://github.com/sjl/gundo.vim.git"
-repo["youcompleteme"]="https://github.com/Valloric/YouCompleteMe.git"
 
+# vim ide plugins
+repo["cvim"]="https://github.com:Maxlufs/c.vim.git"
+# vim autocompletion plugin
+repo["youcompleteme"]="https://github.com/Valloric/YouCompleteMe.git"
 # vim snipmate dependecies
 repo["snipmate"]="https://github.com/garbas/vim-snipmate.git"
 repo["tlib"]="https://github.com/tomtom/tlib_vim.git"
@@ -148,10 +156,10 @@ find $DOTFILEDIR -name '*~' -delete
 
 # Clean up broken symlinks
 #=========================================================================#
-echo ">>> Cleaning up broken symlinks to dotfiles_old/..." 
+echo ">>> Cleaning up broken symlinks to dotfiles_old/..."
 shopt -s dotglob # list hidden files
 
-# if [ -L $OLDDIR ]; then rm $OLDDIR; fi 
+# if [ -L $OLDDIR ]; then rm $OLDDIR; fi
 # There's the problem of .dotfiles_old become a link
 if [ ! -d $OLDDIR ]; then mkdir -p $OLDDIR; fi
 
@@ -178,10 +186,10 @@ echo "----------------------------------------------------------------------"
 ###########################################################################
 # 03. Backing up old dotfiles
 #=========================================================================#
-echo ">>> Backing up old dotfiles to dotfiles_old/..." 
+echo ">>> Backing up old dotfiles to dotfiles_old/..."
 for f in $DOTFILEDIR/*
 do
-    # if [ \( -L $f \) -a \( ! -e $f \) ] 
+    # if [ \( -L $f \) -a \( ! -e $f \) ]
     # if file is a symlink and its broken
     if [ -e ~/.$(basename "$f") ]
     then
@@ -207,38 +215,37 @@ echo "----------------------------------------------------------------------"
 #=========================================================================#
 # Install pathogen
 #=========================================================================#
-MSG=">>> Installing [Pathogen] for Vim..." 
+MSG=">>> Installing [Pathogen] for Vim..."
 printf "$MSG"
 
 mkdir -p $VIMDIR/autoload $VIMDIR/bundle;
 
-if [ ! -f "$VIMDIR/autoload/pathogen.vim" ]
-then
-    if [ ! $(which curl) ]; then sudo apt-get install -y curl; fi;
+if [ ! -f "$VIMDIR/autoload/pathogen.vim" ]; then
+	if [ ! $(which curl) ]; then sudo apt-get install -y curl; fi
 	curl -Sso $VIMDIR/autoload/pathogen.vim $PATHOGENREPO
-    log_msg "[Installation competed.]" "YELLOW" "$MSG"
-    printf " <\n"
+	log_msg "[Installation completed.]" "YELLOW" "$MSG"
+	printf " <\n"
 	#echo "                      [Pathogen] installation completed. <<<"
 else
 	#echo "                           [Pathogen] already installed. <<<"
-    log_msg "[Already up-to-date]" "GREEN" "$MSG"
-    printf " <\n"
+	log_msg "[Already up-to-date]" "GREEN" "$MSG"
+	printf " <\n"
 fi
 echo
 
 # Install vim plugins with git submodules
 #=========================================================================#
 # cd .dotfiles/
-# echo ">>> Installing Submodule plugins for Vim... <<<" 
+# echo ">>> Installing Submodule plugins for Vim... <<<"
 #
-# # Had to remove .git/index to make sure submodules work 
+# # Had to remove .git/index to make sure submodules work
 # # when vim/bundles dir is deleted.
 # rm -f .git/index
-# 
+#
 # for i in "${!repo[@]}"      # support quotes for repo names w/ space in it.
 # do
-#     echo -ne "  > Installing [$i]... \t" 
-#     # if [ \( -d $BUNDLEDIR/$i \) -a "$(ls -A $BUNDLEDIR/$i)" ]  
+#     echo -ne "  > Installing [$i]... \t"
+#     # if [ \( -d $BUNDLEDIR/$i \) -a "$(ls -A $BUNDLEDIR/$i)" ]
 #     # check if plugin dir exists
 #     # and there is something inside the plugin dir
 #     files=$(shopt -s nullglob dotglob; echo $BUNDLEDIR/$i/*)
@@ -256,30 +263,30 @@ echo
 #         echo "installation done <"
 #     fi
 # done
-# 
+#
 # # git submodule -q init # combined into one
 # git submodule -q update --init
-# 
+#
 
 # Install vim plugins
 #=========================================================================#
 cd $BUNDLEDIR
-echo ">>> Installing Submodule plugins for Vim..." 
-for i in "${!repo[@]}"    
+echo ">>> Installing Submodule plugins for Vim..."
+for i in "${!repo[@]}"
     # support quotes for repo names w/ space in it.
 do
-    MSG="  > Installing [$i]... " 
+    MSG="  > Installing [$i]... "
     printf "$MSG"
-    # if [ \( -d $BUNDLEDIR/$i \) -a "$(ls -A $BUNDLEDIR/$i)" ]  
+    # if [ \( -d $BUNDLEDIR/$i \) -a "$(ls -A $BUNDLEDIR/$i)" ]
     # check if plugin dir exists
     # and there is something inside the plugin dir
     files=$(shopt -s nullglob dotglob; echo $i/*)
     if (( ${#files} )) # if there is something inside $i dir
     then
-        cd $i 
+        cd $i
         git pull -q                 # quite mode
         if [ "$(git diff HEAD)" ]; then
-            git reset --hard -q origin/master 
+            git reset --hard -q origin/master
             # My misunderstanding of git pull.
             # need to use git reset in order to copy from commit to working dir
             log_msg "[Update done]" "YELLOW" "$MSG"
@@ -299,13 +306,13 @@ done
 log_msg "[OK]" "GREEN" ""
 printf " <<<\n"
 ###########################################################################
-    
+
 echo "----------------------------------------------------------------------"
 
 ###########################################################################
 # 05. Creating symlinks
 #=========================================================================#
-echo ">>> Creating symbolic links..." 
+echo ">>> Creating symbolic links..."
 
 shopt -s extglob
 # had to use extended pattern matching syntax in bash
@@ -331,44 +338,44 @@ echo "----------------------------------------------------------------------"
 cd $DOTFILEDIR
 defaultMsg="automatic backup"
 
-echo ">>> Backing up to dotfiles.git..." 
+echo ">>> Backing up to dotfiles.git..."
 git add .
 git add -u
 echo "  > Changes to be committed:"
-# git status -s   
+# git status -s
 # print out all modified files with indent, awk might be easier
-# need git config color.status always to pipe color to sed. 
+# need git config color.status always to pipe color to sed.
 # But may have problems with vim-fugitive
-git status -s | sed 's/^/    /' 
+git status -s | sed 's/^/    /'
 
 if [[ $(git diff HEAD) ]]; then
     n=1
     while [ $n -le 3 ]; do
         read -p "  > Do you wish to back up to GitHub this time? [Y/n] " yn
         case $yn in
-            [Yy]*|"" ) 
-                echo "  > Please type in your commit message:" 
+            [Yy]*|"" )
+                echo "  > Please type in your commit message:"
                 read -e -i "$defaultMsg" -p "    " subject # commit msg subject
-                echo "  > Please type in your commit message body: [Press EOF to submit]" 
+                echo "  > Please type in your commit message body: [Press EOF to submit]"
                 echo
                 body=$(</dev/stdin)                        # commit msg body
                 echo
                 # git commit both subject and body
-                git commit -m "$subject" -m "$body" | sed 's/^/  > /' 
+                git commit -m "$subject" -m "$body" | sed 's/^/  > /'
                 # git push
                 log_msg "[OK]" "GREEN" ""
                 printf " <<<\n"
                 break;;
-            [Nn]* ) 
+            [Nn]* )
                 break;;
-            * ) 
+            * )
                 #echo "Unknown input"
                 n=$(( n+1 ))
                 ;;
         esac
     done
 else
-    echo "Nothing to commit (working directory clean)" 
+    echo "Nothing to commit (working directory clean)"
     log_msg "[OK]" "GREEN" ""
     printf " <<<\n"
 fi
